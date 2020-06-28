65518116eef5d8a4447c83caccd757b3b9c566fa
George Noto Issa <georgenoto@gmail.com>
2020-06-28 11:34:14 -0400

    mi primer commit

---
+++ Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Genero, GeneroDTO>().ReverseMap();
+            //Esto debido a que voy a recibir de post GeneroDTO y voy a transforma a Genero, para guardar en mi BD
+            CreateMap<GeneroCreacionDTO, Genero>().ReverseMap();
+        }
+    }
+}
